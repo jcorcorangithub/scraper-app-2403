858b160d94d4e191ea99b40ff8d7ea912ace3523
James Corcoran <jpcsoccer1234@gmail.com>
2022-04-07 09:45:38 -0400

    request seems to be working but am not sure how to grab the data and save the pdf

--- verraPuppeteer.js
+++ verraPuppeteer.js
@@ -25,37 +25,26 @@ async function scrapePDF(){
     // await page.waitForSelector(pdfButton);
     // await page.click(pdfButton);
 
-    axios.post('https://registry.verra.org/uiapi/resource/resource/search?$skip=0&count=true&$format=pdf&$exportFileName=allprojects.pdf', {
-        // firstName: 'Fred',
-        // lastName: 'Flintstone'
-      })
+    axios.post('https://registry.verra.org/uiapi/resource/resource/search?$skip=0&count=true&$format=pdf&$exportFileName=allprojects.pdf',{})
       .then(function (response) {
-        console.log(response);
+        // console.log(response);
+        // const fileNames = fs.readdirSync('/Users/jamescorcoran/Desktop/scraper-app-2403/downloads/verraPDF');
+        // const fileData = fs.readFileSync(`/Users/jamescorcoran/Desktop/scraper-app-2403/downloads/verraPDF/${fileNames[0]}`);
+        // Apify.setValue('MY-PDF', response.data, { contentType: 'application/pdf'});
+        fs.writeFileSync('/Users/jamescorcoran/Desktop/scraper-app-2403/downloads/verraPDF.pdf', response.data, (err) => {
+            if (err)
+              console.log(err);
+            else {
+              console.log("File written successfully\n");
+              console.log(response.data);
+            //   console.log("The written has the following contents:");
+            //   console.log(fs.readFileSync("/Users/jamescorcoran/Desktop/scraper-app-2403/downloads/verraPDF", "utf8"));
+            }
+          });
       })
       .catch(function (error) {
         console.log(error);
       });
-
-    // axios("https://registry.verra.org/uiapi/resource/resource/search?$skip=0&count=true&$format=pdf&$exportFileName=allprojects.pdf", {
-    //     "headers": {
-    //         "accept": "application/pdf",
-    //         "accept-language": "en-US,en;q=0.9",
-    //         "cache-control": "no-cache",
-    //         "content-type": "application/json",
-    //         "pragma": "no-cache",
-    //         "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Google Chrome\";v=\"99\"",
-    //         "sec-ch-ua-mobile": "?0",
-    //         "sec-ch-ua-platform": "\"macOS\"",
-    //         "sec-fetch-dest": "empty",
-    //         "sec-fetch-mode": "cors",
-    //         "sec-fetch-site": "same-origin",
-    //         "cookie": "ASPSESSIONIDAGTASAQS=FHCEBHGAMGJDPBKCIHKPHJEA; fpestid=i8UrwnBZ6Y3WOI67ryUEVU6WhUU-akt5di-UhvYSr-AkgjsGoH6ROQLksiA9gPM75D5Efw; _ga=GA1.2.738807081.1647904552; ASPSESSIONIDAETCQCRS=OBHHOOPBADHAFGFEHJJHPDAH; ASPSESSIONIDSGRTRRBR=EIIOCPJCHBHAACOIIAFKEJIC",
-    //         "Referer": "https://registry.verra.org/app/search/VCS",
-    //         "Referrer-Policy": "strict-origin-when-cross-origin"
-    //     },
-    //     "body": "{\"program\":\"VCS\",\"resourceStatuses\":[\"VCS_EX_CRD_PRD_VER_REQUESTED\",\"VCS_EX_CRD_PRD_REQUESTED\",\"VCS_EX_REGISTERED\",\"VCS_EX_REG_VER_APPR_REQUESTED\",\"VCS_EX_REGISTRATION_REQUESTED\",\"VCS_EX_REJ\",\"VCS_EX_UNDER_DEVELOPMENT_CLD\",\"VCS_EX_UNDER_DEVELOPMENT_OPN\",\"VCS_EX_UNDER_VALIDATION_CLD\",\"VCS_EX_UNDER_VALIDATION_OPN\",\"VCS_EX_CRED_TRANS_FRM_OTHER_PROG\",\"VCS_EX_WITHDRAWN\"]}",
-    //     "method": "POST"
-    //     });
 
 
     // const fileNames = fs.readdirSync('/Users/jamescorcoran/Desktop/scraper-app-2403/downloads/verraPDF');
