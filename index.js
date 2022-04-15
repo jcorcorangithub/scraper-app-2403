83e47aa190ad7bb2299c9caf082de9eba83ce302
James Corcoran <jpcsoccer1234@gmail.com>
2022-04-15 17:46:36 -0400

    the code on index.js is related to the information provided on the link on the top of the file. Also tried a different way of fetching on verraPuppeteer file

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// import myString from './verraPuppeteer'
+// const fileSaver = require('utils/fileSaver');
+// const fileReader = require('utils/fileReader');
+// const { downloadFileRequest } = require('https://registry.verra.org/uiapi/resource/resource/search?$skip=0&count=true&$format=pdf&$exportFileName=allprojects.pdf');
+// // import * as types from './actionTypes';
+// const types = require('./actionTypes');
+
+// const downloadFile = id => async dispatch => {
+//   try {
+//     dispatch({
+//       type: types.DOWNLOAD_FILE_REQUEST,
+//     });
+
+//     const { data } = await downloadFileRequest(id);
+    
+//     await fileSaver(data, 'fileName.txt');
+
+//     dispatch({
+//       type: types.DOWNLOAD_FILE_SUCCESS,
+//     });
+
+//     alert('File downloaded successfully!');
+//   } catch (error) {
+//     dispatch({
+//       type: types.DOWNLOAD_FILE_ERROR,
+//     });
+    
+//     if (error.response) {
+//       try {
+//         const { data } = error.response;
+//         // Read file
+//         const file = await fileReader(data);
+//         // Parse content and retrieve 'message'
+//         const { message } = JSON.parse(file);
+
+//         alert(message);
+//       } catch (readError) {
+//         // Show a dummy error if sth goes wrong while retrieving 'message'
+//         alert('Something went wrong while downloading this file');
+//       }
+//     } else {
+//       alert('Something went wrong while downloading this file');
+//     }
+//   }
+// };
--- verraPuppeteer.js
+++ verraPuppeteer.js
@@ -2,17 +2,48 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 // const pdf = require("pdf-parse");
 const axios = require('axios');
+const { PdfReader } = require("pdfreader");
+const fetch = require("node-fetch");
 
 async function scrapePDF(){
-    const verraProjectsUrl = 'https://registry.verra.org/app/search/VCS';
+  ///////////////////////////////////////////////////////////////////////////////////////////
+  ///this was for programmitcally going to the page and clicking the search button to produce the pdf
+  ///////////////////////////////////////////////////////////////////////////////////////////
+    // const verraProjectsUrl = 'https://registry.verra.org/app/search/VCS';
     
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
+    // const browser = await puppeteer.launch({headless: false});
+    // const page = await browser.newPage();
 
-    await page.goto(verraProjectsUrl);
-    const searchButton = '#verra-application > div > apx-search-page > div > apx-search-container > div > div.card-body > div > div.col.p-0.ml-2 > apx-search-selection-criteria > div > form > div.card-footer.fixed-bottom.bg-transparent.border-0.search-card > div > button:nth-child(1)';
-    await page.waitForSelector(searchButton);
-    page.click(searchButton);
+    // await page.goto(verraProjectsUrl);
+    // const searchButton = '#verra-application > div > apx-search-page > div > apx-search-container > div > div.card-body > div > div.col.p-0.ml-2 > apx-search-selection-criteria > div > form > div.card-footer.fixed-bottom.bg-transparent.border-0.search-card > div > button:nth-child(1)';
+    // await page.waitForSelector(searchButton);
+    // page.click(searchButton);
+  ///////////////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////////////////
+
+    const response = 
+    await fetch("https://registry.verra.org/uiapi/resource/resource/search?$skip=0&count=true&$format=pdf&$exportFileName=allprojects.pdf", {
+      "headers": {
+        "accept": "application/pdf",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        "content-type": "application/json",
+        "pragma": "no-cache",
+        "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Google Chrome\";v=\"99\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"macOS\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "cookie": "ASPSESSIONIDAGTASAQS=FHCEBHGAMGJDPBKCIHKPHJEA; fpestid=i8UrwnBZ6Y3WOI67ryUEVU6WhUU-akt5di-UhvYSr-AkgjsGoH6ROQLksiA9gPM75D5Efw; _ga=GA1.2.738807081.1647904552; ASPSESSIONIDAETCQCRS=OBHHOOPBADHAFGFEHJJHPDAH; ASPSESSIONIDSGRTRRBR=EIIOCPJCHBHAACOIIAFKEJIC",
+        "Referer": "https://registry.verra.org/app/search/VCS",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+      },
+      "body": "{\"program\":\"VCS\",\"resourceStatuses\":[\"VCS_EX_CRD_PRD_VER_REQUESTED\",\"VCS_EX_CRD_PRD_REQUESTED\",\"VCS_EX_REGISTERED\",\"VCS_EX_REG_VER_APPR_REQUESTED\",\"VCS_EX_REGISTRATION_REQUESTED\",\"VCS_EX_REJ\",\"VCS_EX_UNDER_DEVELOPMENT_CLD\",\"VCS_EX_UNDER_DEVELOPMENT_OPN\",\"VCS_EX_UNDER_VALIDATION_CLD\",\"VCS_EX_UNDER_VALIDATION_OPN\",\"VCS_EX_CRED_TRANS_FRM_OTHER_PROG\",\"VCS_EX_WITHDRAWN\"]}",
+      "method": "POST"
+    });
+
+    // console.log(JSON.stringify(response.body));
 
 
     //////////////////////////////////////////////////////////////////////////////////////
@@ -24,38 +55,36 @@ async function scrapePDF(){
     // // const pdfButton = '//*[@id="verra-application"]/div/apx-search-page/div/apx-search-container/div/div[2]/div/div[2]/apx-project-search-results/div/apx-search-results-header/div/button[2]'
     // await page.waitForSelector(pdfButton);
     // await page.click(pdfButton);
+    
+    // axios.post('https://registry.verra.org/uiapi/resource/resource/search?$skip=0&count=true&$format=pdf&$exportFileName=allprojects.pdf', {})
+    //   .then(function (response) {
+    //     console.log(response.data);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    // });
 
-    axios.post('https://registry.verra.org/uiapi/resource/resource/search?$skip=0&count=true&$format=pdf&$exportFileName=allprojects.pdf',{})
-      .then(function (response) {
-        // console.log(response);
-        // const fileNames = fs.readdirSync('/Users/jamescorcoran/Desktop/scraper-app-2403/downloads/verraPDF');
+      // const fileNames = fs.readdirSync('/Users/jamescorcoran/Desktop/scraper-app-2403/downloads/verraPDF');
         // const fileData = fs.readFileSync(`/Users/jamescorcoran/Desktop/scraper-app-2403/downloads/verraPDF/${fileNames[0]}`);
         // Apify.setValue('MY-PDF', response.data, { contentType: 'application/pdf'});
-        fs.writeFileSync('/Users/jamescorcoran/Desktop/scraper-app-2403/downloads/verraPDF.pdf', response.data, (err) => {
-            if (err)
-              console.log(err);
-            else {
-              console.log("File written successfully\n");
-              console.log(response.data);
-            //   console.log("The written has the following contents:");
-            //   console.log(fs.readFileSync("/Users/jamescorcoran/Desktop/scraper-app-2403/downloads/verraPDF", "utf8"));
-            }
-          });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
 
+        //look to see if i can pass response.data into pdf reader 
+        //then read the tables in the pdf 
+        // const data = "/downloads/allprojects.pdf";
+    /////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////////////////////////
+////////////this was how I was going to try to "direct the downloaded" pdf after clicking on the download icon
+//////////////////////////////////////////////////////////////////////////////////////
     // const fileNames = fs.readdirSync('/Users/jamescorcoran/Desktop/scraper-app-2403/downloads/verraPDF');
     // const fileData = fs.readFileSync(`/Users/jamescorcoran/Desktop/scraper-app-2403/downloads/verraPDF/${fileNames[0]}`);
 
     // await Apify.setValue('MY-PDF', fileData, { contentType: 'application/pdf'});
 
 
-
-    //////////////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
 
     
     // await browser.close();
